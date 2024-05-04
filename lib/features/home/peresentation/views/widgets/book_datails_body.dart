c67817eb20c491419237288fb42901ceee1fb3e2
Megoadro <sayedkhalifa086@gmail.com>
2024-05-04 20:20:29 +0300

    Add Title And Subtitle And AppRating

--- lib/features/home/peresentation/views/widgets/book_datails_body.dart
+++ lib/features/home/peresentation/views/widgets/book_datails_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/styles.dart';
 import 'package:bookly/features/home/peresentation/views/widgets/best_seller_image.dart';
 import 'package:bookly/features/home/peresentation/views/widgets/book_details_custom_app_bar.dart';
+import 'package:bookly/features/home/peresentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsBody extends StatelessWidget {
@@ -8,16 +10,36 @@ class BookDetailsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return  Padding(
-      padding:  const EdgeInsets.symmetric(horizontal: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           const BookDetailsCustomAppBar(),
-          const SizedBox(height: 33,),
+          const SizedBox(
+            height: 33,
+          ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width*.14),
+            padding: EdgeInsets.symmetric(horizontal: width * .14),
             child: const BestSellerImage(),
           ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle16,
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const BookRating(),
         ],
       ),
     );
--- lib/features/home/peresentation/views/widgets/book_details_custom_app_bar.dart
+++ lib/features/home/peresentation/views/widgets/book_details_custom_app_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookDetailsCustomAppBar extends StatelessWidget {
   const BookDetailsCustomAppBar({super.key});
