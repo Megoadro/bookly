8b20040f06c5a4b899e7524aaf37228e274207c2
Megoadro <sayedkhalifa086@gmail.com>
2024-05-06 14:38:01 +0300

    mainAxis Done

--- lib/features/home/peresentation/views/widgets/book_rating.dart
+++ lib/features/home/peresentation/views/widgets/book_rating.dart
@@ -9,6 +9,7 @@ class BookRating extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
