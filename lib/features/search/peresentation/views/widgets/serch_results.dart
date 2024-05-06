f0c44dc88bff44d1a8cc6382e21aaff36992f6a7
Megoadro <sayedkhalifa086@gmail.com>
2024-05-06 17:55:28 +0300

    Serch View UI Done

--- lib/features/search/peresentation/views/widgets/serch_results.dart
+++ lib/features/search/peresentation/views/widgets/serch_results.dart
@@ -1,13 +1,13 @@
 import 'package:bookly/features/home/peresentation/views/widgets/best_seller_item.dart';
 import 'package:flutter/material.dart';
 
-class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+class SearchResults extends StatelessWidget {
+  const SearchResults({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 1),
       child: ListView.builder(
         padding: EdgeInsets.zero,
         itemCount: 10,
--- lib/features/search/peresentation/views/widgets/serch_view_body.dart
+++ lib/features/search/peresentation/views/widgets/serch_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly/features/search/peresentation/views/widgets/custom_text_feild.dart';
+import 'package:bookly/features/search/peresentation/views/widgets/serch_results.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SerchViewBody extends StatelessWidget {
   const SerchViewBody({super.key});
@@ -15,6 +17,8 @@ class SerchViewBody extends StatelessWidget {
             height: 40,
           ),
           CustomTextField(),
+           SizedBox(height: 20,),
+           Expanded(child: SearchResults(),),
         ],
       ),
     );
