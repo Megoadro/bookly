d8d908218bb920ede3b788767e76d67ce36f659d
Megoadro <sayedkhalifa086@gmail.com>
2024-05-12 18:26:30 +0300

    create home repo And HomeRepo Imls And Failuer Class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,1 +1,8 @@
+import 'package:bookly/core/errors/failuer.dart';
+import 'package:bookly/features/home/data/models/book_model/item.dart';
+import 'package:dartz/dartz.dart';
 
+abstract class HomeRepo {
+  Future<Either<Failuer, List<Item>>> fetchHomeData();
+  Future<Either<Failuer, List<Item>>> fetchBookDetailsData();
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,1 +1,19 @@
+import 'package:bookly/core/errors/failuer.dart';
+import 'package:bookly/features/home/data/models/book_model/item.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 
+class HomeRepoImpls implements HomeRepo{
+  @override
+  Future<Either<Failuer, List<Item>>> fetchBookDetailsData() {
+    // TODO: implement fetchBookDetailsData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failuer, List<Item>>> fetchHomeData() {
+    // TODO: implement fetchHomeData
+    throw UnimplementedError();
+  }
+
+}
