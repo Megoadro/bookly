c62b506852586260bbedf3c8b54b228dfe5ce38c
Megoadro <sayedkhalifa086@gmail.com>
2024-05-12 20:46:38 +0300

    Add Dio Package And Create Get Method At  Api Service

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,8 +1,9 @@
 import 'package:bookly/core/errors/failuer.dart';
-import 'package:bookly/features/home/data/models/book_model/item.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failuer, List<Item>>> fetchBookItemData();
-  Future<Either<Failuer, List<Item>>> fetchBestSellerData();
+  //All Methods There Witthout Impls(Yes Thats abs class) .. Only Describe Name And Type Of  Methods
+  Future<Either<Failuer, List<BookModel>>> fetchBookItemData();
+  Future<Either<Failuer, List<BookModel>>> fetchBestSellerData();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,17 +1,17 @@
 import 'package:bookly/core/errors/failuer.dart';
-import 'package:bookly/features/home/data/models/book_model/item.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpls implements HomeRepo{
   @override
-  Future<Either<Failuer, List<Item>>> fetchBestSellerData() {
+  Future<Either<Failuer, List<BookModel>>> fetchBestSellerData() {
     // TODO: implement fetchBestSellerData
     throw UnimplementedError();
   }
 
   @override
-  Future<Either<Failuer, List<Item>>> fetchBookItemData() {
+  Future<Either<Failuer, List<BookModel>>> fetchBookItemData() {
     // TODO: implement fetchBookItemData
     throw UnimplementedError();
   }
