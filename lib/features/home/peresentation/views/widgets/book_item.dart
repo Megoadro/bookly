d0ba8b49d6a7b1b929af4fa65d087ad34a5a0b86
Megoadro <sayedkhalifa086@gmail.com>
2024-05-18 17:07:19 +0300

    Add ClipRRect To CashedNetworkImage

--- lib/features/home/peresentation/views/widgets/book_item.dart
+++ lib/features/home/peresentation/views/widgets/book_item.dart
@@ -9,16 +9,19 @@ class BookItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(right: 15),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * .28,
-        child: AspectRatio(
-          aspectRatio: 3 / 4,
-          child: CachedNetworkImage(
-            height: 200,
-            imageUrl: imageUrl,
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-            fit: BoxFit.fill,
-
+      child: ClipRRect(
+        borderRadius:BorderRadius.circular(16) ,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * .28,
+          child: AspectRatio(
+            aspectRatio: 3 / 4,
+            child: CachedNetworkImage(
+              height: 200,
+              imageUrl: imageUrl,
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              fit: BoxFit.fill,
+        
+            ),
           ),
         ),
       ),
